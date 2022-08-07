15817684be6d53d2d8abd87889c478493540399f
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-08-07 23:17:02 +0700

    add controller to update user and to update and create product and also to search by name

--- back-end/controller/controller.go
+++ back-end/controller/controller.go
@@ -16,8 +16,12 @@ func NewMux(userRepo *repository.UserRepository, productRepo *repository.Product
 	m.Handle("/api/v1/user", router.GetUserByID(userRepo))
 	m.Handle("/api/v1/user/create", router.CreateUser(userRepo))
 	m.Handle("/api/v1/user/delete", router.DeleteUserByID(userRepo))
+	m.Handle("/api/v1/user/update", router.UpdateUser(userRepo))
 	m.Handle("/api/v1/products", router.GetAllProduct(productRepo))
 	m.Handle("/api/v1/product", router.GetProductID(productRepo))
 	m.Handle("/api/v1/product/delete", router.DeleteProductByID(productRepo))
+	m.Handle("/api/v1/product/create", router.CreateProduct(productRepo))
+	m.Handle("/api/v1/product/update", router.UpdateProduct(productRepo))
+	m.Handle("/api/v1/product/search", router.GetProductByName(productRepo))
 	return m
 }
