7ce9cdfa3353d6d07f17b3e632842dfcb46822be
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2022-08-07 23:14:58 +0700

    changes on database

--- back-end/models/models.go
+++ back-end/models/models.go
@@ -3,10 +3,13 @@ package models
 import "database/sql"
 
 type Product struct {
-	ID       uint           `db:"id"`
-	Name     string         `db:"name"`
-	Price    uint           `db:"price"`
-	ImageLoc sql.NullString `db:"image_loc"`
+	ID              uint           `db:"id"`
+	Name            string         `db:"name"`
+	Price           uint           `db:"price"`
+	Description     string         `db:"desc"`
+	CurrentQuantity uint           `db:"cur_quantity"`
+	Quantity        uint           `db:"quantity"`
+	ImageLoc        sql.NullString `db:"image_loc"`
 }
 
 type Users struct {
@@ -15,4 +18,10 @@ type Users struct {
 	Password string         `db:"password"`
 	Email    string         `db:"email"`
 	ImageLoc sql.NullString `db:"image_loc"`
+}
+
+type Roles struct {
+	ID     uint   `db:"id"`
+	UserID uint   `db:"user_id"`
+	Roles  string `db:"name"`
 }
