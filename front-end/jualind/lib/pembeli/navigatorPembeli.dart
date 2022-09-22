9ea9495106503e878f15986dd6ef9ffc58eac8a5
Redoqx <ridho.pmswa@gmail.com>
2022-09-22 18:54:31 +0700

    feat: jualind mobile app

---
+++ front-end/jualind/lib/pembeli/navigatorPembeli.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NavBeli extends StatefulWidget {
+  const NavBeli({super.key});
+
+  @override
+  State<NavBeli> createState() => _NavBeliState();
+}
+
+class _NavBeliState extends State<NavBeli> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
